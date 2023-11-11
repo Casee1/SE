bd917a2d119ea4be02eccc63d9ce0a258e343812
Darius Tandea <66829985+Darimen@users.noreply.github.com>
2023-11-11 17:09:43 +0200

    idk

---
+++ src/main/java/se/proj/SEProject/Review.java
@@ -0,0 +1,17 @@
+package se.proj.SEProject;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    private ObjectId _id;
+    private String body;
+
+}
